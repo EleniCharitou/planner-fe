cc8c1870dda7f5e72cc017851c5494f07212b9ef
eleni <elenixar7879@gmail.com>
2025-08-01 19:59:56 +0200

    Homepage content

--- src/new_pages/Homepage.tsx
+++ src/new_pages/Homepage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { MapPin, Calendar, BookOpen, Video } from "lucide-react";
+import { FaArrowRight } from "react-icons/fa";
+import { MdCardTravel, MdOutlineModeOfTravel } from "react-icons/md";
 
 function Homepage() {
   return (
@@ -14,53 +16,78 @@ function Homepage() {
         </div>
 
         {/* Main content */}
-        <div className="grid grid-cols-4 grid-rows-3 gap-4 h-96 min-w-screen/2">
+        <div className="grid grid-cols-4 grid-rows-3 gap-4 h-fit min-w-screen/2">
           {/* First rectangle: 2 columns, 2 rows */}
           <div className="col-span-2 row-span-2 bg-teal-500 rounded-lg shadow-lg flex p-6">
             <div className="text-white text-left">
-              <h2 className="text-xl font-semibold text-left mb-2">
-                Travel planner & Memory maker
+              <h2 className="text-2xl font-semibold text-left mb-3">
+                Your space to plan and track your next trip
               </h2>
-              <ul>
-                <li>1.</li>
-                <li>2.</li>
-                <li>3.</li>
+              Everythning in one place for your next trip planning for you and
+              your friends.
+              <ul className="list-disc font-normal text-white p-4">
+                <li>
+                  Create a list with your ideas and the places you want to
+                  visit.
+                </li>
+                <li>
+                  Add extra info on each place or activity you want [location,
+                  tickets, openning hours, etc.].
+                </li>
+                <li> Organize everything in a Kanban board.</li>
+                <li>
+                  Keep track of the activities you did and which of them you
+                  want to do next time.
+                </li>
+                <li>
+                  Rate every activity, add photos, write your feelings, and a
+                  funny or memorable story from this trip.
+                </li>
               </ul>
             </div>
           </div>
 
           {/* Second rectangle: 2 columns, 1 row */}
-          <div
+          <button
             className="col-span-2 row-span-1 bg-teal-500 rounded-lg shadow-lg flex items-center justify-center
                         hover:bg-teal-800 hover:cursor-pointer"
           >
             <div className="text-white text-left">
-              <h2 className="text-xl font-semibold">Plan your trip</h2>
+              <h2 className="text-xl font-semibold">
+                Plan your trip
+                <MdCardTravel className="inline-flex pl-2" size="35px" />
+              </h2>
             </div>
-          </div>
+          </button>
 
           {/* Third rectangle: 2 columns, 1 row */}
-          <div className="col-span-2 row-span-1 bg-teal-500 rounded-lg shadow-lg flex items-center justify-center">
-            <div className="text-white text-center">
-              <h2 className="text-lg font-semibold mb-1">Your trips</h2>
+          <button
+            className="col-span-2 row-span-1 bg-teal-500 rounded-lg shadow-lg flex items-center justify-center
+          hover:bg-gray-600 hover:cursor-not-allowed"
+          >
+            <div className="inline-flex justify-center items-center text-white text-center">
+              <h2 className="text-2xl font-semibold mb-1">Your trips</h2>
             </div>
-          </div>
+          </button>
 
           {/* Fourth rectangle: 3 columns, 1 row */}
           <div className="col-span-3 row-span-1 bg-teal-500 rounded-lg shadow-lg flex p-6">
             <div className="text-white text-left">
-              <h2 className="text-lg font-semibold">Travel trips & tricks</h2>
+              <h2 className="text-2xl font-semibold">Travel trips & tricks</h2>
             </div>
           </div>
 
           {/* Fifth rectangle: 1 column, 1 row */}
-          <div className="col-span-1 row-span-1 bg-teal-500 rounded-lg shadow-lg flex items-center justify-center">
+          <button
+            className="col-span-1 row-span-1 bg-teal-500 rounded-lg shadow-lg flex items-center justify-center
+          hover:bg-gray-500 hover:cursor-not-allowed"
+          >
             <div className="text-white text-center">
-              <h2 className="text-lg font-semibold m-2 p-8">
+              <h2 className="text-2xl font-semibold m-2 p-8">
                 Generate your <br /> memory video
               </h2>
             </div>
-          </div>
+          </button>
         </div>
       </div>
     </div>
